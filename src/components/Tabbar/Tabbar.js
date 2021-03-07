9da4d41f37f626da92c9f15b4272ad8aa585325a
Denis Vlasov <wd055@mail.ru>
2021-03-07 03:35:51 +0300

    PIC_47: Добавить компоненту Tabbar

---
+++ src/components/Tabbar/Tabbar.js
@@ -0,0 +1,36 @@
+/**
+ * @class
+ * Компонента Tabbar
+ */
+class Tabbar {
+    /**
+     * Создает экземпляр Tabbar
+     *
+     * @constructor
+     * @this  {Tabbar}
+     * @param {Object[]} context {icons:[{iconClasses='', size=28, src='logo.png', href}]}
+     * @param parent
+     */
+	constructor() {
+		this.template = Handlebars.templates['Tabbar.hbs'];
+		this.context = {
+			icons: [
+				{
+					iconClasses: '',
+					size: 28,
+					src: 'logo.png',
+				},
+			],
+		};
+	}
+
+	setContext(context) {
+		this.context = context;
+	}
+
+	render() {
+		return this.template(this.context);
+	}
+}
+
+export default Tabbar;
