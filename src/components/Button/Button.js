13618b85d1672a37813aaad70c59276907eb4d66
wh75er <wh75er@gmail.com>
2021-03-07 04:13:20 +0300

    PIC-28: Добавить контекст к компоненту кнопки в метод отрисовки

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,15 +3,10 @@ const buttonTemplate = 'Button.hbs'
 class Button {
     constructor() {
         this.template = Handlebars.templates[buttonTemplate];
-        this.context = {'text': 'button test'}
     }
 
-    setContext(context) {
-        this.context = context
-    }
-
-    render() {
-        return this.template(this.context)
+    render(context) {
+        return this.template(context)
     }
 }
 
