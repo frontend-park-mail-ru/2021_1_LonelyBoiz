cd8c3e6b85ab21c82dda8df32d68ea77f7e7b195
wh75er <wh75er@gmail.com>
2021-03-08 01:10:06 +0300

    PIC-29: Добавить компонент отображения опций

---
+++ src/components/Select/Select.js
@@ -0,0 +1,27 @@
+const select = 'Select.hbs'
+
+/**
+ * @class
+ * Компонента выбора опций
+ */
+class Select {
+	/**
+	 * Создает экземпляр Select
+	 *
+	 * @constructor
+	 * @this  {Select}
+	 */
+	constructor() {
+		this.template = Handlebars.templates[select];
+	}
+
+	/**
+	 * Отображает компонент выбора опций
+	 */
+	render(context) {
+		return this.template(context);
+	}
+
+}
+
+export default Select;
