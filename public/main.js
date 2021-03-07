77fbd668933eb8ef49f177508874669443c83bba
wh75er <wh75er@gmail.com>
2021-03-07 22:26:49 +0300

    PIC-28: Исправить стиль кода

--- public/main.js
+++ public/main.js
@@ -4,15 +4,11 @@ import Form from '../src/components/Form/Form.js';
 import LoginView from '../src/view/LoginView/LoginView.js';
 import MainLabel from "../src/components/MainLabel/MainLabel.js";
 
-let app = document.getElementById("app");
+const app = document.getElementById("app");
 
-let button = new Button();
-let input = new Input();
-let form = new Form();
+const form = new Form();
 
-//app.innerHTML = button.render({'text': 'Hello there'});
-//app.innerHTML = input.render({'type': 'password', 'placeholder': 'Пароль'});
-let renderedForm = form.render({
+const renderedForm = form.render({
   'inputs': [
       new Input().render({'type': 'text', 'placeholder': 'Почта'}),
       new Input().render({'type': 'password', 'placeholder': 'Пароль'}),
@@ -20,11 +16,11 @@ let renderedForm = form.render({
   'button': new Button().render({'text': 'Вход'})
 });
 
-let render_value = new LoginView().render({
+const renderValue = new LoginView().render({
     'header': new MainLabel().render({'text': 'PICKle'}),
     'form': renderedForm,
     'signupHref': 'signup',
     'error': 'Неправильно введен логин или пароль.Введите пароль заново. что-то пошло не так'
     })
 
-app.innerHTML = render_value
+app.innerHTML = renderValue
