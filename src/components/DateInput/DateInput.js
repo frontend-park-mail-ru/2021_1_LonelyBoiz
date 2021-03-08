3582445cc5ab5cbe9973095d67904a3713b43386
wh75er <wh75er@gmail.com>
2021-03-08 03:41:19 +0300

    PIC-29: Добавить компоненту для ввода даты

---
+++ src/components/DateInput/DateInput.js
@@ -0,0 +1,27 @@
+const dateInput = 'DateInput.hbs'
+
+/**
+ * @class
+ * Компонента установки даты
+ */
+class DateInput {
+	/**
+	 * Создает экземпляр DateInput
+	 *
+	 * @constructor
+	 * @this  {DateInput}
+	 */
+	constructor() {
+		this.template = Handlebars.templates[dateInput];
+	}
+
+	/**
+	 * Отображает компонент установки даты
+	 */
+	render(context) {
+		return this.template(context);
+	}
+
+}
+
+export default DateInput;
