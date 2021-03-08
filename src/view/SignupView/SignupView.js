6de722f95a8318af278be90e7052d127b33aabb8
wh75er <wh75er@gmail.com>
2021-03-08 03:41:39 +0300

    Добавить страницу для регистрации

---
+++ src/view/SignupView/SignupView.js
@@ -0,0 +1,27 @@
+const signupViewTemplate = 'SignupView.hbs'
+
+/**
+ * @class
+ * Страница логина
+ */
+class SignupView {
+    /**
+     * Создает экземпляр signupView
+     *
+     * @constructor
+     * @this  {SignupView}
+     */
+    constructor() {
+        this.template = Handlebars.templates[signupViewTemplate];
+    }
+
+    /**
+     * Отображает страницу регистрации
+     * @param {Object} context - форма с полями ввода и кнопкой
+     */
+    render(context) {
+        return this.template(context);
+    }
+}
+
+export default SignupView;
