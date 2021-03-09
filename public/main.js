198c025ec4cd4de1aac512609389ff0264e612b2
wh75er <wh75er@gmail.com>
2021-03-09 23:09:46 +0300

    PIC-52: Обновить main

--- public/main.js
+++ public/main.js
@@ -1,22 +1,7 @@
-import Button from '../src/components/Button/Button.js';
-import Input from '../src/components/Input/Input.js';
-import Form from '../src/components/Form/Form.js';
-import MainLabel from '../src/components/MainLabel/MainLabel.js';
-import DateInput from '../src/components/DateInput/DateInput.js';
-import Select from '../src/components/Select/Select.js'
-import LoginView from '../src/view/LoginView/LoginView.js';
-import SignupView from '../src/view/SignupView/SignupView.js';
+import LoginController from '../src/controllers/LoginController.js'
+import SignupController from "../src/controllers/SignupController.js";
 
 import EventBus from '../src/utils/eventBus.js';
 import Events from '../src/consts/events.js';
 
-
-const signupView = new SignupView({'signupHref': 'login', 'error': 'error occurred'}).show();
-
-function onButtonSubmit(data) {
-    console.log('hello world')
-}
-
-EventBus.connect(Events.formSubmitted, onButtonSubmit)
-const button = document.querySelector('.button-block__button')
-button.addEventListener('click', (e) => {e.preventDefault(); EventBus.emit(Events.formSubmitted);})
+const signupController = new SignupController()
