261a22ccdf4227a8b0adf426643c087c9fe1a82d
wh75er <wh75er@gmail.com>
2021-03-09 23:08:47 +0300

    PIC-52: Добавить модель для логина и регистрации

---
+++ src/models/AuthModel.js
@@ -0,0 +1,27 @@
+import { postRequest } from "../utils/requests.js";
+
+/**
+ * Отправляет данные для входа
+ *
+ * @param {Object} data данные для логина
+ * @return {Promise}
+ */
+export function sendLoginData(data) {
+    return postRequest('/login', data)
+        .then(response => {
+            return response.json()
+        });
+}
+
+/**
+ * Отправляет данные для регистрации
+ *
+ * @param {Object} data данные для регистрации
+ * @return {Promise}
+ */
+export function sendSignUpData(data) {
+    return postRequest('/signup', data)
+        .then(response => {
+            return response.json()
+        });
+}
