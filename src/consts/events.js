96cdc7ed87c9542b5b00abe89f0f7ad702810490
wh75er <wh75er@gmail.com>
2021-03-09 23:06:49 +0300

    PIC-52: Добавить события для валидации и роутинга

--- src/consts/events.js
+++ src/consts/events.js
@@ -1,10 +1,14 @@
 const Events = {
     formSubmitted: 'formSubmitted',
+    formError: 'formError',
     mailValidationFailed: 'mailValidationFailed',
     nameValidationFailed: 'nameValidationFailed',
     passwordValidationFailed: 'passwordValidationFailed',
     passwordMatchFailed: 'passwordMatchFailed',
-    dateValidationFailed: 'dateValidationFailed'
+    dateValidationFailed: 'dateValidationFailed',
+    routeToHomePage: 'routeToHomePage',
+    routeToLoginPage: 'routeToLoginPage',
+    routeToSignupPage: 'routeToSignupPage',
 };
 
 export default Events;
