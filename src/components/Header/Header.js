e58f1ce5f8191c374cdb83373bf466f08421ce14
Denis Vlasov <wd055@mail.ru>
2021-03-10 07:56:04 +0300

    PIC-25: Добавить иконки и настроку иконок header`а

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import Tabbar from '../Tabbar/Tabbar.js';
 import MainLabel from '../MainLabel/MainLabel.js';
+import headerIcons from '../../consts/headerIcons.js';
+
 /**
  * @class
  * Компонента Header
@@ -10,11 +12,14 @@ class Header {
      *
      * @constructor
      * @this  {Header}
-     * @param {Object} context
+     * @param {Object} context {activeIcon:headerIcons}
      */
     constructor(context) {
         this.template = Handlebars.templates['Header.hbs'];
-        this.context = context || {};
+        this.context = context || { activeIcon: '' };
+        if (!this.context.activeIcon) {
+            this.context.activeIcon = '';
+        }
     }
 
     /**
@@ -22,16 +27,22 @@ class Header {
      * @returns {string} Построенный компонент
      */
     render() {
+        const activeIcon = this.context.activeIcon;
         this.context.Tabbar = new Tabbar({
             icons: [
-                { iconClasses: '', size: 28, src: 'icon/home_fill.svg' },
+                { iconClasses: '', size: 28, src: `icon/home_${activeIcon === headerIcons.home ? 'fill' : 'stroke'}.svg` },
                 {
                     iconClasses: '',
                     size: 28,
-                    src: 'icon/send_message_stroke.svg'
+                    src: `icon/send_message_${activeIcon === headerIcons.send_message ? 'fill' : 'stroke'}.svg`
                 },
-                { iconClasses: '', size: 28, src: 'icon/search_stroke.svg' },
-                { iconClasses: '', size: 28, src: 'icon/like_stroke.svg' },
+                {
+                    iconClasses: '',
+                    size: 28,
+                    src: `icon/location_${activeIcon === headerIcons.location ? 'fill' : 'stroke'}.svg`
+                },
+                { iconClasses: '', size: 28, src: `icon/like_${activeIcon === headerIcons.like ? 'fill' : 'stroke'}.svg` },
+                { iconClasses: '', size: 28, src: 'icon/settings.svg' },
                 { iconClasses: 'avatar', size: 28, src: 'img/logo.png' }
             ]
         }).render();
