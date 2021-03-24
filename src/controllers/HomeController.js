4b184a31a970d638e38cff0cd9eeb2f8c82818f5
Denis Vlasov <wd055@mail.ru>
2021-03-24 05:23:49 +0300

    PIC-83: Отображать дату от возраста, а не константу

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -32,22 +32,30 @@ class HomeController extends BaseController {
                     eventBus.emit(Events.formError);
                 } else {
                     console.log(json);
+                    let user = {
+                        name: json.name,
+                        avatar: window.localStorage.getItem('u-avatar'),
+                        geo: json.geo,
+                        city: json.city,
+                        instagram: json.instagram,
+                        description: json.description
+                    };
+                    if (json.birthday) {
+                        user.age = Math.floor(
+                            (new Date() - new Date(json.birthday)) /
+                                (1000 * 3600 * 24 * 365)
+                        );
+                    }
                     const card = new Card({
-                        user: {
-                            name: json.name,
-                            age: 20,
-                            avatar: window.localStorage.getItem('u-avatar'),
-                            geo: json.geo,
-                            city: json.city,
-                            instagram: json.instagram,
-                            description: json.description
-                        },
+                        user: user,
                         photos: [window.localStorage.getItem('u-avatar')],
                         horizontal: true
                     }).render();
                     document.getElementById('home-card').innerHTML = card;
 
-                    const imgs = document.getElementsByClassName('photo-block__img');
+                    const imgs = document.getElementsByClassName(
+                        'photo-block__img'
+                    );
                     if (imgs.length > 0) {
                         const rgbBackground = getAverageRGB(imgs[0]);
                         imgs[0].style.backgroundColor = `rgb(${rgbBackground.r},${rgbBackground.g},${rgbBackground.b})`;
