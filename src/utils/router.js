ed08e8560fa30197e84a568db592d166ae369284
wh75er <wh75er@gmail.com>
2021-03-26 21:02:14 +0300

    PIC-85: Расширить роутер:
    Добавить изменение URL
    Добавить историю
    Добавить работу с query параметрами

--- src/utils/router.js
+++ src/utils/router.js
@@ -15,6 +15,7 @@ class Router {
      */
     constructor() {
         this.routes = new Map();
+        this.currPath = null;
         this.controller = null;
         eventBus.connect(Events.routeChange, this.changeRoute.bind(this));
 
@@ -24,20 +25,53 @@ class Router {
                 this.changeRoute(e.target.dataset.routlink);
             }
         });
+
+        window.onpopstate = function(e) {
+            this.setupPage(e.target.location.pathname + e.target.location.search);
+        }.bind(this);
+    }
+
+    /**
+     *
+     *  Точка входа в роутер.
+     * Отображает страницу с запрошенным url
+     *
+     */
+    start() {
+        this.setupPage(window.location.pathname + window.location.search);
+        window.history.replaceState(null, null, this.currPath);
     }
 
     /**
      * Изменить путь
      *
-     * @params {String} route путь до
+     * @params {String} path путь до
      */
-    changeRoute(route) {
+    changeRoute(path) {
+        this.setupPage(path);
+        if (window.location.pathname + window.location.search !== this.currPath) {
+            window.history.pushState(null, null, this.currPath);
+        } else {
+            window.history.replaceState(null, null, this.currPath);
+        }
+    }
+
+    /**
+     * Вспомогательная функция для изменения пути
+     *
+     * @params {String} path путь до
+     */
+    setupPage(path) {
+        this.currPath = path;
+
+        path = this.parsePath(path);
+        const route = path.route;
+
         let controller = this.routes.get(route);
-        let chosenRoute = route;
 
         if (!controller) {
             controller = this.routes.get(Routes.homeRoute);
-            chosenRoute = Routes.homeRoute;
+            this.currPath = Routes.homeRoute;
             if (!controller) {
                 return;
             }
@@ -50,12 +84,13 @@ class Router {
             this.controller = controller;
         }
 
-        if (!window.localStorage.getItem('u-id') && chosenRoute !== Routes.loginRoute && chosenRoute !== Routes.signupRoute) {
-            chosenRoute = Routes.loginRoute;
-            this.controller = this.routes.get(chosenRoute);
+        if (!window.localStorage.getItem('u-id') &&
+        this.parsePath(this.currPath).route !== Routes.loginRoute &&
+        this.parsePath(this.currPath).route !== Routes.signupRoute) {
+            this.currPath = Routes.loginRoute;
+            this.controller = this.routes.get(this.parsePath(this.currPath).route);
         }
 
-        document.location.hash = chosenRoute;
         this.controller.start();
     }
 
@@ -67,6 +102,22 @@ class Router {
      */
     addRoute(route, controller) {
         this.routes.set(route, controller);
+    }
+
+    /**
+     * Разъеденить роут и query параметры
+     *
+     * @params {String} path путь страницы
+     *
+     * @return {Object} возвращает объект вида {route: String, query: String}
+     */
+    parsePath(path) {
+        path = path.split('?');
+        if (path.length > 1) {
+            return { route: path[0], query: path[1] };
+        }
+
+        return { route: path[0], query: '' };
     }
 }
 
