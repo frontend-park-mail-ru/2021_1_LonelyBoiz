186353bca57dbe3def82df4684b8979005670d60
wh75er <wh75er@gmail.com>
2021-04-15 19:03:22 +0300

    PIC-106: Наладить отправку сообщений по сокету

--- src/utils/WebSocketListener.ts
+++ src/utils/WebSocketListener.ts
@@ -11,6 +11,7 @@ class WebSocketListener {
     static instance: WebSocketListener = null;
     webSocket: WebSocket = null;
     handlers: IHandlers = {};
+    pingInterval: ReturnType<typeof setInterval> = null;
 
     constructor() {
         this.handlers.message = addMessageHandler;
@@ -28,33 +29,42 @@ class WebSocketListener {
     listen() {
         if (this.webSocket === null) {
             this.webSocket = new WebSocket(websocketLocation);
-            this.webSocket.addEventListener('message', this.onMessage);
+            this.webSocket.addEventListener('message', this.onMessageEvent.bind(this));
+            this.pingInterval = setInterval(this.ping.bind(this), 30 * 1000);
         }
     }
 
     stop() {
         if (this.webSocket !== null) {
-            this.webSocket.removeEventListener('message', this.onMessage);
+            this.webSocket.removeEventListener('message', this.onMessageEvent);
+            this.webSocket.close();
             this.webSocket = null;
+
+            clearInterval(this.pingInterval);
+            this.pingInterval = null;
         }
     }
 
-    onMessage(e: Context) {
-        console.log('Message from socket: ', e);
-        if (this.handlers[e.data.type]) {
-            this.handlers[e.data.type](e.data.obj);
+    ping() {
+        this.webSocket.send('{type: \'ping\', obj: {}}');
+    }
+
+    onMessageEvent(e: Context) {
+        const data = JSON.parse(e.data);
+        if (this.handlers[data.type]) {
+            this.handlers[data.type](data.obj);
         }
     }
 }
 
 export interface IMessageSocketData {
     messageId: number,
-    authorId: number,
+    authorId?: number,
     chatId: number,
-    text: string,
-    date: Date,
-    reactionId: number,
-    messageOrder: number,
+    text?: string,
+    date?: Date,
+    reactionId?: number,
+    messageOrder?: number,
 }
 
 export interface IChatSocketData {
