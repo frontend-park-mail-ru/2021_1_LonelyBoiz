09cfbc94d5392653465ed0541f6d5e5e318c6ba9
wh75er <wh75er@gmail.com>
2021-05-26 00:32:33 +0300

    PIC-153: Сделать микроизменения

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -1,7 +1,7 @@
 const url = 'lepick.ru';
-const backendLocation = 'https://' + url + '/backend';
-export const websocketLocation = 'wss://' + url + '/backend/ws';
+const backendLocation = 'https://' + url + '/api';
+export const websocketLocation = 'wss://' + url + '/api/ws';
 export const imageBackendLocation = 'https://' + url + '/images';
-export const imageStorageLocation = 'https://lepick-images.s3.eu-central-1.amazonaws.com';
+export const imageStorageLocation = 'https://' + url + '/image-storage';
 
 export default backendLocation;
